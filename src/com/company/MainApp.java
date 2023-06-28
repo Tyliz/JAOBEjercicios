bcfc3e1fe9e128c46ab4906426461c517f3db5ee
TylizRv <caosavenger@gmail.com>
2023-06-28 14:45:32 +0100

    Ejercicio 1

---
+++ src/com/company/MainApp.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import com.ejercicio1.EjercicioUno;
+
+public class MainApp {
+    public static void main(String[] args) {
+        // Ejercicio 1
+        EjercicioUno.ImprimirTiposDatos();
+    }
+}
---
+++ src/com/ejercicio1/EjercicioUno.java
@@ -0,0 +1,24 @@
+package com.ejercicio1;
+
+public class EjercicioUno {
+    public static void ImprimirTiposDatos() {
+        byte numeroByte = 1;
+        System.out.println(numeroByte + " es de tipo: byte");
+        int numeroInt = 2;
+        System.out.println(numeroInt + " es de tipo: int");
+        long nueroLong = 3;
+        System.out.println(nueroLong + " es de tipo: Long");
+        double numeroDouble = 4.1D;
+        System.out.println(numeroDouble + " es de tipo: double");
+        float numeroFloat = 5.5F;
+        System.out.println(numeroFloat + " es de tipo: float");
+        boolean esFalso = true;
+        System.out.println(esFalso + " es de tipo: boolean");
+
+        String cadena = "Texto es de tipo: String";
+        System.out.println(cadena);
+
+        char letra = 'A';
+        System.out.println(letra + " es de tipo: char");
+    }
+}
